b3173b98c75ef3b5e92825c5fb6ae8e4c97af4df
eemmmmma <emagnin@udesa.edu.ar>
2025-10-27 18:09:45 -0300

    avanzamos con lo que podemos

---
+++ magninPereyraG3-expo/src/firebase/config.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdYZx_9HpZUlLyBfRlXV6vOWIpnwxAu_I",
+  authDomain: "magnin-pereyra-g3.firebaseapp.com",
+  projectId: "magnin-pereyra-g3",
+  //storageBucket: "magnin-pereyra-g3.firebasestorage.app",
+  storageBucket: "magnin-pereyra-g3.appspot.com",
+  messagingSenderId: "471192441541",
+  appId: "1:471192441541:web:48b4c4e499f30b2da5d6ac"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
