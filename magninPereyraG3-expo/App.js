db863016bdec9d5da98adcd76fe7446d860c4366
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2025-11-03 17:25:41 -0300

    logre que se vean las cosas

--- magninPereyraG3-expo/App.js
+++ magninPereyraG3-expo/App.js
@@ -1,12 +1,9 @@
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack"; // no lw gusat etso 
-import Home from "./src/screens/Home";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
-import Profile from "./src/screens/Profile";
-import NewPost from "./src/screens/NewPost";
 import Header from "./src/components/Header";
 
 const Stack = createNativeStackNavigator();
@@ -15,15 +12,11 @@ function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="NewPost" component={NewPost} />
-      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="Header" component={Header} />
     </Stack.Navigator>
     </NavigationContainer>
   );
 }
-// hola no anda diosgit add
 export default App;
+
---
+++ magninPereyraG3-expo/src/components/HomeTab.js
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+function HomeTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+      name='New Post'
+      component={NewPost}
+      options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="post" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+export default HomeTab;
