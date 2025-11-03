289fe01fba7cacc63dcd79f8101cbdb4565731b6
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2025-11-03 19:42:11 -0300

    faltaba import en profile

--- magninPereyraG3-expo/src/screens/Profile.js
+++ magninPereyraG3-expo/src/screens/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { auth }  from '../firebase/config';
 
 function Profile({ navigation }) {
   return (
